cc038cebe9d6f949cd09dde6c80b993bc6b8ec06
mashinson <mashinson1@gmail.com>
2017-02-04 05:28:18 +0200

    Sorts

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20170128_SortersDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            double[] numbers = new double[1000];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = rnd.NextDouble();
+            }
+
+            
+            TestSortMethod(new InsertSorter(numbers));          
+            TestSortMethod(new BoobleSorter(numbers));
+            TestSortMethod(new QuickSorter(numbers));
+            TestSortMethod(new MergeSort(numbers));
+
+            double[] numbers1 = new double[10000];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers1[i] = i;
+            }
+
+            TestSortMethod(new InsertSorter(numbers1));
+            TestSortMethod(new BoobleSorter(numbers1));
+            TestSortMethod(new QuickSorter(numbers1));
+            TestSortMethod(new MergeSort(numbers1));
+
+            double[] numbers2 = new double[1000000];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers2[i] = numbers.Length-i;
+            }
+
+            TestSortMethod(new InsertSorter(numbers2));
+            TestSortMethod(new BoobleSorter(numbers2));
+            TestSortMethod(new QuickSorter(numbers2));
+            TestSortMethod(new MergeSort(numbers2));
+
+            Console.ReadKey();
+        }
+
+        private static void TestSortMethod(Sorter s)
+        {
+            double start = Environment.TickCount;
+            s.Sort();
+            double time = Environment.TickCount - start;
+            s.Time = time;
+            Console.WriteLine(s);
+            Console.WriteLine();
+      
+        }
+    }
+}
---
+++ Sorter.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20170128_SortersDemo
+{
+    abstract class Sorter
+    {
+        public const int DEF_SIZE = 10000;
+
+        #region ctors
+
+        public Sorter(int size = DEF_SIZE)
+        {
+
+            _numbers = new double[size];
+            // инициализация с помощью Random !!!
+        }
+
+        public Sorter(double[] numbers)
+        {
+            _numbers = (double[])numbers.Clone();
+        }
+
+        #endregion
+
+        public abstract string SortName { get; }
+        public abstract void Sort();
+
+        protected bool IsLargeThan(double a, double b)
+        {
+            ++_compareCount;
+
+            return a > b;
+        }
+
+        protected void Swap(ref double a, ref double b)
+        {
+            ++_swapCount;
+
+            double c = b;
+            b = a;
+            a = c;
+        }
+
+        public double Time
+        {
+            get
+            {
+                return _time;
+            }
+            set 
+            {
+                _time = value;
+
+            }
+
+        }
+
+        public override string ToString()
+        {
+            return string.Format("SortName: {0}, compareCount: {1}, swapCount: {2}, time: {3}", SortName, _compareCount, _swapCount, Time);
+        }
+
+        protected double[] _numbers = null;
+
+        private uint _compareCount = 0;
+        private uint _swapCount = 0;
+        private double _time = 0;
+    }
+}
